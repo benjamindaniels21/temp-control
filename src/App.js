08ca687ab66601dfff0bf62b3b502d4d3b6b0a96
Ben Daniels <benjamindaniels21@gmail.com>
2022-09-22 10:03:05 -0400

    buttons and temp reading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="app-container">
-      <div className="temperature-display-container"></div>
+      <div className="temperature-display-container">
+        <div className="temperature-display">70°F</div>
+      </div>
+      <div className="button-container">
+        <button>+</button>
+        <button>-</button>
+      </div>
     </div>
   );
 }
